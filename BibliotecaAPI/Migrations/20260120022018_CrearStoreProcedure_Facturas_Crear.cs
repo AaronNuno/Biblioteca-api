910561e76f3fab21aa7eea9caf5b9d30ed6e2979
Aaron Nuno <aaron_a_7@hotmail.com>
2026-01-19 20:25:31 -0600

    sp Facturas_Crear

---
+++ BibliotecaAPI/Migrations/20260120022018_CrearStoreProcedure_Facturas_Crear.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibliotecaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearStoreProcedure_Facturas_Crear : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE PROCEDURE Facturas_Crear
+
+ @fechaInicio datetime,
+ @fechaFin datetime 
+
+AS
+BEGIN
+
+SET NOCOUNT ON ;
+
+DECLARE @montoPorCadaPeticion decimal (4,4) = 1.0/2
+ 
+INSERT INTO Facturas(UsuarioId,Monto,FechaEmision,FechaLimiteDePago, Pagada)
+SELECT
+UsuarioId,
+COUNT(*) * @montoPorCadaPeticion AS Monto,
+GETDATE() AS FechaEmision,
+DATEADD(d,60,GETDATE()) AS FechaLimiteDePago,
+0 as Pagada
+FROM Peticiones
+INNER  JOIN LlavesAPI
+ON LlavesAPI.Id = Peticiones.LlaveId
+WHERE LlavesAPI.TipoLlave !=1 AND FechaPeticion >= @fechaInicio  AND FechaPeticion < @fechaFin
+GROUP BY UsuarioId
+
+INSERT INTO FacturasEmitida (Mes,Año)
+SELECT
+    CASE MONTH(GETDATE())
+     WHEN 1 THEN 12
+     ELSE MONTH(GETDATE()) -1 END AS MES,
+     
+     CASE MONTH(GETDATE())
+     WHEN 1 THEN YEAR(GETDATE()) -1
+     ELSE YEAR (GETDATE()) END AS Año
+
+     END ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE Facturas_Crear");
+        }
+    }
+}
